241d7d40bafc0a3fd435a8174dfdf6e1bff5111c
Will Reed <wilreed@cisco.com>
2020-05-05 12:03:32 -0700

    normalize

--- segmentation_models_pytorch/base/model.py
+++ segmentation_models_pytorch/base/model.py
@@ -4,30 +4,38 @@ from . import initialization as init
 
 class SegmentationModel(torch.nn.Module):
 
+    mean = torch.tensor([0.485, 0.456, 0.406])
+    std = torch.tensor([0.229, 0.224, 0.225])
+
     def initialize(self):
         init.initialize_decoder(self.decoder)
         init.initialize_head(self.segmentation_head)
         if self.classification_head is not None:
             init.initialize_head(self.classification_head)
 
+    def normalize(self, x):
+        # print(x.shape)
+        # x = x.permute(0, 2, 3, 1)
+        # print(x.shape)
+        x = x / 255.
+        # x = x - mean
+        # x = x / std
+        for xi in range(3):
+            x[0][xi] = x[0][xi] - self.mean[xi]
+            x[0][xi] = x[0][xi] / self.std[xi]
+        # x = x.permute(0, 3, 1, 2)
+        # print(x.shape)
+        return x
+
     def forward(self, x):
         """Sequentially pass `x` trough model`s encoder, decoder and heads"""
+        # import pdb;pdb.set_trace()
+        x = self.normalize(x)
         features = self.encoder(x)
         decoder_output = self.decoder(*features)
 
         masks = self.segmentation_head(decoder_output)
-
-        # import pdb;pdb.set_trace()
-
-        if False:
-            w = 128
-            h = 128
-            x2 = torch.zeros(masks.shape)
-            y = 3
-            shift_x = 3
-            # mask_2[:h-y, :w-x] = mask[y:y+(h-y), x:x+(w-x):]
-            x2[:,:,y:y+(h-y), shift_x:shift_x+(w-shift_x)] = masks[:,:,:h-y, :w-shift_x]
-            masks = x2
+        masks = masks * 255.
 
         if self.classification_head is not None:
             labels = self.classification_head(features[-1])
